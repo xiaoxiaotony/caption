762aaca81d9190dc555c6dc784ae99a200c855e1
xiaoxiaotony <1028622605@qq.com>
2016-03-28 10:43:31 +0800

    修改代码格式化

--- src/main/java/com/sdlc/caption/App.java
+++ src/main/java/com/sdlc/caption/App.java
@@ -6,14 +6,13 @@ import org.apache.log4j.Logger;
  * Hello world!
  *
  */
-public class App 
-{
-	
+public class App {
+
 	private static final Logger logger = Logger.getLogger(App.class);
-    public static void main( String[] args )
-    {
-    	logger.info("caption info .....");
-        System.out.println( "Hello World!" );
-        logger.info("app main finish .......");
-    }
+
+	public static void main(String[] args) {
+		logger.info("caption info .....");
+		System.out.println("Hello World!");
+		logger.info("app main finish .......");
+	}
 }
