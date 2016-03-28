7bdc87cd28227b6758f4f1e51be3ecbe8de42722
xiaoxiaotony <1028622605@qq.com>
2016-03-28 10:15:05 +0800

    修改logger配置

--- src/main/java/com/sdlc/caption/App.java
+++ src/main/java/com/sdlc/caption/App.java
@@ -1,13 +1,18 @@
 package com.sdlc.caption;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+	private static final Logger logger = Logger.getLogger(App.class);
     public static void main( String[] args )
     {
+    	logger.info("caption info .....");
         System.out.println( "Hello World!" );
     }
 }
